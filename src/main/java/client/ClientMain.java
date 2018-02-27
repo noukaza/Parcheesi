1bcbe4b72965dd1dd60cd0db079fab84f4f1aa09
Noureddine Amrane <noureddine.amrane@etu.u-pec.fr>
2018-02-27 18:49:47 +0100

    and the handler

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -2,14 +2,10 @@ package client;
 
 import client.core.Connect;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 public class ClientMain {
 
     public static void main(String[] args) {
-        new Connect("localhost",1235);
+		new Connect("localhost", 1234);
     }
 
 }
